80f186c22a7c60d548364046fe4dabaa72ffc7a4
DinaraKelyamova30 <kelyamova30@gmail.com>
2019-11-09 12:09:09 +0200

    js

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
 let a = prompt('Write password "likeJS"');
-let count = 0;
-while (a != 'likeJS' && count < 4) {
-    if (a != 'likeJS' && count == 0){
-    alert('Неправильний проль. Залишилось 4 спроби!');
-    count++;
+let count = 4;
+while(a!='likeJS' && count>0){
+    alert(`Залишилось ${count} спроб`);
     a = prompt('Write password "likeJS"');
+    if (a == 'likeJS'){
+        count = 0;
     }
-    if (a != 'likeJS' && count == 1) {
-        alert('Неправильний проль. Залишилось 3 спроби!');
-        count++;
-        a = prompt('Write password "likeJS"');
-    } 
-    if (a != 'likeJS' && count == 2) {
-        alert('Неправильний проль. Залишилось 2 спроби!');
-        count++;
-        a = prompt('Write password "likeJS"');
-    }
-    if (a != 'likeJS' && count == 3) {
-        alert('Неправильний проль. Залишилось 1 спроби!');
-        count++;
-        a = prompt('Write password "likeJS"');
-    }
-    if (a != 'likeJS' && count == 4) {
-        alert('Будь ласка спробуйте пізніше');
-    }
+    count --;
 }
-if (a = 'likeJS' ){
-    alert('Вітємо на сайті');
+if(a == 'likeJS'){
+    alert('welcome');
+} else {
+    alert('try again later');
 }
